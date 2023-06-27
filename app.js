f90707c5659bd7ace2fc44e48546b6e464ecc5f0
jayasree687 <137481237+jayasree687@users.noreply.github.com>
2023-06-27 16:00:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var renderer, scene, camera, floor, car, envMap,clock,animationMixers;
 var isCarPlaced = false;
 
 function setupRenderer(rendererCanvas) {
+  const MOUSE = { LEFT: 0, MIDDLE: 1, RIGHT: 2, ROTATE: 0, DOLLY: 1, PAN: 2 };
+	const TOUCH = { ROTATE: 0, PAN: 1, DOLLY_PAN: 2, DOLLY_ROTATE: 3 };
   const width = rendererCanvas.width;
   const height = rendererCanvas.height;
 
