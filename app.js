f48de9641f66a7a808838adbd845cce71b29806c
jayasree687 <137481237+jayasree687@users.noreply.github.com>
2023-06-30 10:42:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // ====== Imports ======
-
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
 import OnirixSDK from "https://unpkg.com/@onirix/ar-engine-sdk@1.3.1/dist/ox-sdk.esm.js";
 import * as THREE from "https://cdn.skypack.dev/three@0.127.0";
 import { GLTFLoader } from "https://unpkg.com/three@0.154.0/examples/jsm/loaders/GLTFLoader.js";
